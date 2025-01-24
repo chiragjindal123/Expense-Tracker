5cd426c679468f63a7bfe972386de135b0a1cbcf
CHIRAG JINDAL <94543614+chiragjindal123@users.noreply.github.com>
2025-01-25 01:28:58 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middlewares
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' })); // Allow requests from your frontend
+app.use(cors({ origin: 'https://expense-tracker-frontend-rqfy.onrender.com' })); // Allow requests from your frontend
 
 // Routes
 try {
